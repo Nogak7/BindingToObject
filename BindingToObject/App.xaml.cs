16413c94c9a492b2537a7316b65708c786019ca3
Home <Home@LAPTOP-2E6V6ACM>
2023-02-14 18:42:11 +0200

    Add project files.

---
+++ BindingToObject/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BindingToObject;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BindingToObject/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace BindingToObject;
+using BindingToObject.Models;
+
+public partial class MainPage : ContentPage
+{
+	public Phone Phones { get; set; } = new Phone("0506665835", "Iphone", "iphone.png");
+
+    public MainPage()
+	{
+		
+		InitializeComponent();
+	}
+
+	
+}
+
