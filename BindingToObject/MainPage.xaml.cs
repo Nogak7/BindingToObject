a72dd79c4bb3c09a903ee96e0d80b544d84c63aa
Home <Home@LAPTOP-2E6V6ACM>
2023-02-21 23:04:49 +0200

    BaseProject

--- BindingToObject/MainPage.xaml.cs
+++ BindingToObject/MainPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿namespace BindingToObject;
-using BindingToObject.Models;
+
 
 public partial class MainPage : ContentPage
 {
-	public Phone Phones { get; set; } = new Phone("0506665835", "Iphone", "iphone.png");
+    
 
     public MainPage()
 	{
-		
-		InitializeComponent();
-	}
+       
 
-	
+        InitializeComponent();
+
+    }
+
+   
 }
 
