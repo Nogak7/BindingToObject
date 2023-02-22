9673485abb5563f1617929061fc140f5baf4cdd6
User <User@LAB3-15>
2023-02-22 10:42:01 +0200

    22

--- BindingToObject/Models/Student.cs
+++ BindingToObject/Models/Student.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BindingToObject.Models
 {
-    internal class Student
+    public class Student:INotifyPropertyChanged              
     {
-        public string Name { get; set; }    
+        public string Name { get; set; }
         public string Image { get; set; }
         public DateTime BirthDate { get; set; }
+        private DateTime currDate;
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public DateTime CurrDate { get => currDate;  set { if(value!=currDate) currDate = value; OnPropertyChanged("CurrDate"); } }
+        public int Age { get => (int)(CurrDate - BirthDate).TotalDays / 365; }
+
+        public Student()
+        {
+            currDate = DateTime.Now;
+        }
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));  
+        }
     }
 }
